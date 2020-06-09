461c783cd70512ff8d6c53b88584a40afed83439
xujun <xujun@gegejia.com>
2020-06-09 10:57:57 +0800

    查看字节码

---
+++ je-jvm/src/main/java/com/sigalhu/je/jvm/bytecode/TestClass.java
@@ -0,0 +1,14 @@
+package com.sigalhu.je.jvm.bytecode;
+
+/**
+ * @author huxujun
+ * @date 2020/3/19
+ */
+public class TestClass {
+
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+}
